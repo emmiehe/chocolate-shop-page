583411e56e3e7b40a0f1007ef4276e5447c92b7e
Emmie He <heemmie@gmail.com>
2020-11-12 19:06:30 -0500

    draft

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// import {Paper} from "@material-ui/core";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Product extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+      </div>
+    );
+  }
+}
+
+class CartLine extends React.Component {
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    return (
+      <div>
+        <h1>{this.props.name}</h1>
+      </div>
+    );
+  }
+}
+
+class Cart extends React.Component {
+  static initState(){
+    return {
+      lines: [
+        {name: "line 1"},
+      ],
+    };
+  }
+
+  constructor(props){
+    super(props);
+    this.state = Cart.initState();
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState(Cart.initState());
+  }
+
+  render(){
+    return (
+      <div>
+        {this.state.lines.map((line) => <CartLine name={line.name}/>)}
+      </div>
+    )
+  }
+}
+
+class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      products: [
+        {name: "Product 1"},
+        {name: "Product 2"},
+        {name: "Product 3"},
+        {name: "Product 4"},
+        {name: "Product 5"},
+        {name: "Product 6"},
+        {name: "Product 7"},
+        {name: "Product 8"},
+        {name: "Product 9"},
+        {name: "Product 10"},
+        {name: "Product 11"},
+        {name: "Product 12"},
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="main">
+        <Cart addItem={this.addItem}/>
+        <div>
+          {this.state.products.map((product) => <Product name={product.name}/>)}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;

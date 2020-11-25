59d781ab0f07a7b8ce476367a95447be8160a1a0
Emmie He <heemmie@gmail.com>
2020-11-24 19:47:02 -0500

    material layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
-// import {Paper} from "@material-ui/core";
+import './App.css';
+import {Card, Button} from "@material-ui/core";
+// import {MDCRipple} from "@material-ui/ripple";
 
 class Product extends React.Component {
   constructor(props){
@@ -8,9 +10,22 @@ class Product extends React.Component {
 
   render(){
     return (
-      <div>
-        <h1>{this.props.name}</h1>
-      </div>
+      <Card className="mdc-card mdc-card--outlined">
+        <div className="mdc-card__primary-action">
+          <div className="mdc-card__media mdc-card__media--square">
+          </div>
+        </div>
+        <h4>{this.props.name}</h4>
+        <p>Hello I am a piece of delicious chocolate please come and eat me  </p>
+        <div className="mdc-card__actions">
+          <div className="button-container mdc-card__action-buttons">
+            <Button className="mdc-button mdc-button--raised mdc-card__action mdc-card__action--button">
+              <span className="mdc-button__label">Add to Cart</span>
+            </Button>
+          </div>
+        </div>
+      </Card>
+
     );
   }
 }
@@ -34,6 +49,17 @@ class Cart extends React.Component {
     return {
       lines: [
         {name: "line 1"},
+        {name: "line 2"},
+        {name: "line 3"},
+        {name: "line 4"},
+        {name: "line 5"},
+        {name: "line 6"},
+        {name: "line 7"},
+        {name: "line 8"},
+        {name: "line 9"},
+        {name: "line 10"},
+        {name: "line 11"},
+        {name: "line 12"},
       ],
     };
   }
@@ -51,7 +77,8 @@ class Cart extends React.Component {
 
   render(){
     return (
-      <div>
+      <div className="cart">
+        <h4>Hi I'm a cart and this is what I have:</h4>
         {this.state.lines.map((line) => <CartLine name={line.name}/>)}
       </div>
     )
@@ -64,18 +91,18 @@ class App extends React.Component {
     super(props);
     this.state = {
       products: [
-        {name: "Product 1"},
-        {name: "Product 2"},
-        {name: "Product 3"},
-        {name: "Product 4"},
-        {name: "Product 5"},
-        {name: "Product 6"},
-        {name: "Product 7"},
-        {name: "Product 8"},
-        {name: "Product 9"},
-        {name: "Product 10"},
-        {name: "Product 11"},
-        {name: "Product 12"},
+        {name: "Product A"},
+        {name: "Product B"},
+        {name: "Product C"},
+        {name: "Product D"},
+        {name: "Product E"},
+        {name: "Product F"},
+        {name: "Product G"},
+        {name: "Product H"},
+        {name: "Product I"},
+        {name: "Product J"},
+        {name: "Product K"},
+        {name: "Product L"},
       ],
     };
   }
@@ -83,10 +110,10 @@ class App extends React.Component {
   render() {
     return (
       <div className="main">
-        <Cart addItem={this.addItem}/>
-        <div>
+        <div className="products">
           {this.state.products.map((product) => <Product name={product.name}/>)}
         </div>
+        <Cart addItem={this.addItem}/>
       </div>
     )
   }

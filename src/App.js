07503f780e116ba5e08343fc52eb29ccf54208b6
Emmie He <heemmie@gmail.com>
2020-12-03 19:49:37 -0800

    fixed sortBy and rearranged code

--- src/App.js
+++ src/App.js
@@ -1,47 +1,20 @@
 import React from "react";
 import './App.css';
 import {Card, Button} from "@material-ui/core";
-import white_1 from "./white-1.jpg";
-import white_2 from "./white-2.jpg";
-import white_3 from "./white-3.jpg";
-import dark_1 from "./dark-1.jpg";
-import dark_2 from "./dark-2.jpg";
-import dark_3 from "./dark-3.jpg";
-import dark_4 from "./dark-4.jpg";
-import dark_5 from "./dark-5.jpg";
-import milk_1 from "./milk-1.jpg";
-import milk_2 from "./milk-2.jpg";
-import milk_3 from "./milk-3.jpg";
-import milk_4 from "./milk-4.jpg";
 
-var all_products = [
-  {name: "Product A", id: 1, img: white_1, price: 4.4, color: "White", size: "individual"},
-  {name: "Product B", id: 2, img: dark_1, price: 9.9, color: "Dark", size: "party"},
-  {name: "Product C", id: 3, img: milk_1, price: 2.2, color: "Milk", size: "party"},
-  {name: "Product D", id: 4, img: white_2, price: 1.1, color: "White", size: "party"},
-  {name: "Product E", id: 5, img: dark_2, price: 5.5, color: "Dark", size: "individual"},
-  {name: "Product F", id: 6, img: milk_2, price: 6.6, color: "Milk", size: "party"},
-  {name: "Product G", id: 7, img: white_3, price: 7.7, color: "White", size: "twin-pack"},
-  {name: "Product H", id: 8, img: dark_3, price: 8.8, color: "Dark", size: "twin-pack"},
-  {name: "Product I", id: 9, img: milk_3, price: 3.3, color: "Milk", size: "twin-pack"},
-  {name: "Product J", id: 10, img: dark_4, price: 10.1, color: "Dark", size: "individual"},
-  {name: "Product K", id: 11, img: milk_4, price: 11.1, color: "Milk", size: "individual"},
-  {name: "Product L", id: 12, img: dark_5, price: 12.2, color: "Dark", size: "party"},
-];
+// name, fn
+export class SortByItem {
+  constructor(name, fn) {
+    this.name = name;
+    this.fn = fn;
+  }
+};
 
-var all_filters = [
-  {name: "color", options: ["", "White", "Dark", "Milk"], value: ""},
-  {name: "size", options: ["", "individual", "party", "twin-pack"], value: ""},
-];
-
+// Individual product
+// Including id, name, img, description, two attributes and a price
 class Product extends React.Component {
   constructor(props){
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(event) {
-    this.props.addToCart(event, this.props);
   }
 
   render(){
@@ -56,10 +29,10 @@ class Product extends React.Component {
         <h4>Price: {this.props.price}</h4>
         <p>Color: {this.props.color}</p>
         <p>Size: {this.props.size}</p>
-        <p>Hello I am a piece of delicious chocolate please come and eat me  </p>
+        <p>{this.props.description}</p>
         <div className="mdc-card__actions">
           <div className="button-container mdc-card__action-buttons">
-            <Button onClick={this.handleClick} className="mdc-button mdc-button--raised mdc-card__action mdc-card__action--button">
+            <Button onClick={(event) => this.props.onAddToCart(event, this.props)} className="mdc-button mdc-button--raised mdc-card__action mdc-card__action--button">
               <span className="mdc-button__label">Add to Cart</span>
             </Button>
           </div>
@@ -72,18 +45,37 @@ class Product extends React.Component {
 class Filter extends React.Component {
   constructor(props){
     super(props);
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  handleClick(option) {
-    this.props.addToFilters(this.props.name, option);
   }
 
   render(){
     return (
       <h2 className="uppercase" style={{width: "100%"}}>{this.props.name}
-        {this.props.options.map((option) => <Button onClick={() => this.handleClick(option)}>{option || "All"}</Button>)}
+        {this.props.options.map((option) => <Button onClick={() => this.props.addToFilters(this.props.name, option)}>{option || "All"}</Button>)}
       </h2>
+    );
+  }
+}
+
+// [<SortByItem> item1, ...]
+class SortBy extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleSelect = this.handleSelect.bind(this);
+  }
+
+  handleSelect(event) {
+    let fn = this.props.sortByItems[parseInt(event.target.value)].fn;
+    this.props.sortBy(fn);
+  }
+
+  render(){
+    return (
+      <div>
+        <span>Sort by: </span>
+        <select onChange={this.handleSelect}>
+          {this.props.sortByItems.map((item, index) => <option value={index}>{item.name}</option>)}
+        </select>
+      </div>
     );
   }
 }
@@ -110,23 +102,23 @@ class CartLine extends React.Component {
   render(){
     return (
       <div>
-        <h5>{this.props.name}  <Button onClick={this.handleDecrease}>-</Button>{this.props.qty}<Button onClick={this.handleIncrease}>+</Button>  {this.props.price}/{this.props.total} <Button onClick={this.handleRemove}>Remove</Button></h5>
+        <h5>{this.props.name}  <Button onClick={this.handleDecrease}>-</Button>{this.props.qty}<Button onClick={this.handleIncrease}>+</Button> {this.props.price}/{this.props.total} <Button onClick={this.handleRemove}>Remove</Button></h5>
       </div>
     );
   }
 }
 
-class App extends React.Component {
+export class App extends React.Component {
 
   constructor(props){
     super(props);
     this.state = {
-      products: all_products,
-      filters: all_filters,  // name, options, value
+      products: this.props.products,  // id, name, img, description, two attributes, price
+      filters: this.props.filters,  // name, options, value
       cart_lines: [],
       total_amount: 0.0,
     };
-    this.addToCart = this.addToCart.bind(this);
+    this.handleAddToCart = this.handleAddToCart.bind(this);
     this.remove = this.remove.bind(this);
     this.increase = this.increase.bind(this);
     this.decrease = this.decrease.bind(this);
@@ -135,7 +127,7 @@ class App extends React.Component {
     this.sortBy = this.sortBy.bind(this);
   }
 
-  addToCart(event, props) {
+  handleAddToCart(event, props) {
     let existing_line = this.state.cart_lines.find(line => line.name === props.name); // perhaps using key is better
     if (!existing_line){
       this.state.cart_lines.push({name: props.name, qty: props.qty, price: props.price, total: props.price});
@@ -150,8 +142,8 @@ class App extends React.Component {
   remove(event, props){
     let existing_line = this.state.cart_lines.find(line => line.name === props.name); // perhaps using key is better
     if (existing_line) {
-      this.state.cart_lines = this.state.cart_lines.filter(line => line.name !== props.name);
-      this.setState({cart_lines: this.state.cart_lines, total_amount: parseFloat((this.state.total_amount - props.total).toFixed(2))});
+      let res_cart_lines = this.state.cart_lines.filter(line => line.name !== props.name);
+      this.setState({cart_lines: res_cart_lines, total_amount: parseFloat((this.state.total_amount - props.total).toFixed(2))});
     }
   }
 
@@ -178,12 +170,12 @@ class App extends React.Component {
   }
 
   applyFilters(){
-    let products = all_products;
+    let products = this.props.products;
     for (let i=0; i< this.state.filters.length; i++){
       let filterName = this.state.filters[i].name;
       let filterValue = this.state.filters[i].value;
       if (filterValue) {
-        console.log(filterValue);
+        // console.log(filterValue);
         products = products.filter(product => product[filterName] === filterValue);
       }
     }
@@ -196,45 +188,33 @@ class App extends React.Component {
       existing_filter.value = option;
     }
     this.setState({filter: this.state.filters});
-    console.log(this.state.filters);
+    // console.log(this.state.filters);
     this.applyFilters();
   }
 
-  sortBy(event){
-    let val = event.target.value;
-    if (val === "select"){
-      this.setState({products: this.state.products.sort((a, b) => a.id - b.id)});
-    } else if (val === "lowToHigh"){
-      this.setState({products: this.state.products.sort((a, b) => a.price - b.price)});
-    } else if (val === "highToLow") {
-      this.setState({products: this.state.products.sort((a, b) => b.price - a.price)});
-    }
+  sortBy(fn){
+    this.setState({products: this.state.products.sort(fn)});
   }
 
   render() {
     return (
       <div className="main">
         <div className="products">
-          {this.state.products.map((product) => <Product name={product.name} img={product.img} price={product.price} color={product.color} size={product.size} total={product.price} qty={1} addToCart={this.addToCart}/>)}
+          {this.state.products.map((product) => <Product key={product.id} name={product.name} img={product.img} description={product.description} price={product.price} color={product.color} size={product.size} total={product.price} qty={1} onAddToCart={this.handleAddToCart}/>)}
         </div>
         <div>
           <h4>Total Amount: {this.state.total_amount}</h4>
           <div className="cart">
-            {this.state.cart_lines.map((line) => <CartLine name={line.name} price={line.price} total={line.total} qty={line.qty} remove={this.remove} increase={this.increase} decrease={this.decrease}/>)}
+            {this.state.cart_lines.map((line) => <CartLine key={line.name} name={line.name} price={line.price} total={line.total} qty={line.qty} remove={this.remove} increase={this.increase} decrease={this.decrease}/>)}
           </div>
         </div>
         <div>
-          {this.state.filters.map((filter) => <Filter name={filter.name} options={filter.options} value={filter.value} addToFilters={this.addToFilters}/>)}
-          <h2>Sort by: </h2>
-          <select name="sortBy" id="sortBy" onChange={this.sortBy}>
-            <option value="select">Select</option>
-            <option value="lowToHigh">Price Low to High</option>
-            <option value="highToLow">Price High to Low</option>
-          </select>
+          {this.state.filters.map((filter) => <Filter key={filter.name} name={filter.name} options={filter.options} value={filter.value} addToFilters={this.addToFilters}/>)}
+          {<SortBy sortByItems={this.props.sortByItems} sortBy={this.sortBy}/>}
         </div>
       </div>
     )
   }
 }
 
-export default App;
+// export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { SortByItem, App } from './App';
 import reportWebVitals from './reportWebVitals';
+import white_1 from "./white-1.jpg";
+import white_2 from "./white-2.jpg";
+import white_3 from "./white-3.jpg";
+import dark_1 from "./dark-1.jpg";
+import dark_2 from "./dark-2.jpg";
+import dark_3 from "./dark-3.jpg";
+import dark_4 from "./dark-4.jpg";
+import dark_5 from "./dark-5.jpg";
+import milk_1 from "./milk-1.jpg";
+import milk_2 from "./milk-2.jpg";
+import milk_3 from "./milk-3.jpg";
+import milk_4 from "./milk-4.jpg";
+
+var dataProducts = [
+  {name: "Product A", id: 1, img: white_1, price: 4.4, color: "White", size: "individual", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product B", id: 2, img: dark_1, price: 9.9, color: "Dark", size: "party", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product C", id: 3, img: milk_1, price: 2.2, color: "Milk", size: "party", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product D", id: 4, img: white_2, price: 1.1, color: "White", size: "party", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product E", id: 5, img: dark_2, price: 5.5, color: "Dark", size: "individual", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product F", id: 6, img: milk_2, price: 6.6, color: "Milk", size: "party", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product G", id: 7, img: white_3, price: 7.7, color: "White", size: "twin-pack", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product H", id: 8, img: dark_3, price: 8.8, color: "Dark", size: "twin-pack", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product I", id: 9, img: milk_3, price: 3.3, color: "Milk", size: "twin-pack", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product J", id: 10, img: dark_4, price: 10.1, color: "Dark", size: "individual", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product K", id: 11, img: milk_4, price: 11.1, color: "Milk", size: "individual", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+  {name: "Product L", id: 12, img: dark_5, price: 12.2, color: "Dark", size: "party", description: "Hello I am a piece of delicious chocolate please come and eat me!"},
+];
+
+var dataFilters = [
+  {name: "color", options: ["", "White", "Dark", "Milk"], value: ""},
+  {name: "size", options: ["", "individual", "party", "twin-pack"], value: ""},
+];
+
+var dataSortByItems = [
+  new SortByItem("Select", (a, b) => a.id - b.id),
+  new SortByItem("Price Low to High", (a, b) => a.price - b.price),
+  new SortByItem("Price High to Low", (a, b) => b.price - a.price),
+];
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App products={dataProducts} filters={dataFilters} sortByItems={dataSortByItems}/>
   </React.StrictMode>,
   document.getElementById('root')
 );
